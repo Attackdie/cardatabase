78a6a6984a2e8ada0173e759433d5a54ba3ef5cd
suminLee <pkpk85@naver.com>
2023-06-21 12:01:22 +0900

    테이블 관의 관계 의존성 연결

--- cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ cardatabase/src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -1,5 +1,7 @@
 package com.packt.cardatabase;
 
+import java.util.Arrays;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,40 +11,43 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.packt.cardatabase.domain.Car;
 import com.packt.cardatabase.domain.CarRepository;
+import com.packt.cardatabase.domain.Owner;
+import com.packt.cardatabase.domain.OwnerRepository;
 
-//2023 06 19 CommandLineRunner h2 데이터베이스 입출력 추가
 @SpringBootApplication
-public class CardatabaseApplication implements CommandLineRunner{
-	private static final Logger logger =
+public class CardatabaseApplication implements CommandLineRunner {	
+	private static final Logger logger = 
 			LoggerFactory.getLogger(CardatabaseApplication.class);
-	
-	//2023 06 19 의존성 주입
-	@Autowired
-	private CarRepository repository;
+	  
+    @Autowired
+    private CarRepository repository;
+    
+    @Autowired
+    private OwnerRepository orepository;
 
+	
 	public static void main(String[] args) {
-		//2023 06 18 로깅 테스트
-		//이 주석을 추가하면 애플리케이션 재시작됨
 		SpringApplication.run(CardatabaseApplication.class, args);
-		logger.info("Application started");
 	}
-	
-	
-	//2023 06 19 
+
 	@Override
 	public void run(String... args) throws Exception {
-		
-		// 이곳에 코드를 추가
-		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2021, 59000, null));
-		repository.save(new Car("Nissan", "Leaf", "white", "SSJ-3002", 2019, 29000, null));
-		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2020, 39000, null));
-		
-		//2023 06 19
-		//모든 자동차를 가져와 콘솔에 로깅
+		// Add owner objects and save these to db 
+		Owner owner1 = new Owner("John" , "Johnson");
+		Owner owner2 = new  Owner("Mary" , "Robinson");
+		orepository.saveAll(Arrays.asList(owner1, owner2));
+
+		// Add car object and link to owners and save these to db
+		Car car1 = new Car("Ford", "Mustang", "Red", 
+				"ADF-1121", 2021, 59000, owner1);
+		Car car2 = new Car("Nissan", "Leaf", "White", 
+				"SSJ-3002", 2019, 29000, owner2);
+		Car car3 = new Car("Toyota", "Prius", "Silver", 
+				"KKO-0212", 2020, 39000, owner2);
+		repository.saveAll(Arrays.asList(car1, car2, car3));
+                  
 		for (Car car : repository.findAll()) {
 			logger.info(car.getBrand() + " " + car.getModel());
-			
 		}
 	}
-
 }
