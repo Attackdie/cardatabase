94d57c03d8287d9c18eb8e6be7fc5de68a90fd7d
Attackdie <pkpk85@naver.com>
2023-06-25 19:42:57 +0900

    테스트 커밋 푸쉬

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let car = '차 종류';
+  return (
+    <div className="App">
+      <div className="black-nav">
+     <h3>카 센터 블로그 테스트11111</h3>
+      </div>
+      <h4>{ car }</h4>
+    </div>
+  );
+}
+
+export default App;
