960ea93d3f0bacfdc398f0ee508ef0847f64b702
Attackdie <pkpk85@naver.com>
2023-07-08 10:46:32 +0900

    AG grid 및 기타 리액트 컴포넌트 연결후 테스트 커밋

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,34 +1,38 @@
 // import React from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-material.css';
 import React, { useState } from 'react';
-// import './App.css';
+import './App.css';
 
 
-//2023 07 07 이벤트 폼 처리 팝업창 구현
-function MyList() {
-  const [text, setText] = useState('');
+
+// //2023 07 07 이벤트 폼 처리 팝업창 구현
+// function MyList() {
+//   const [text, setText] = useState('');
   
-  //입력 요소의 내용이 변경되면 값을 저장
-  const inputChanged = (event) => {
-    setText(event.target.value);
-  }
-  const handleSubmit = (event) => {
-    alert(`You typed: ${text}`);
-    //어이 없는 오류 alert 'You typed: ${text}' X 역따움표를 사용하지 않았을때 뜨는오류 `You typed: ${text}` O
-    // alert('You typed: ${text}'); X
-    event.preventDefault();
-  }
-  return (
-    <form onSubmit={handleSubmit}>
-      <input type="text" onChange={inputChanged}
-      value={text}/>
-      <input type="submit" value="Press me" />
+//   //입력 요소의 내용이 변경되면 값을 저장
+//   const inputChanged = (event) => {
+//     setText(event.target.value);
+//   }
+//   const handleSubmit = (event) => {
+//     alert(`You typed: ${text}`);
+//     //어이 없는 오류 alert 'You typed: ${text}' X 역따움표를 사용하지 않았을때 뜨는오류 `You typed: ${text}` O
+//     // alert('You typed: ${text}'); X
+//     event.preventDefault();
+//   }
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <input type="text" onChange={inputChanged}
+//       value={text}/>
+//       <input type="submit" value="Press me" />
 
-    </form>
-  );
+//     </form>
+//   );
 
-};
+// };
 
-export default MyList;
+// export default MyList;
 
 // //2023 07 07 버튼 카운트 기능 테스트 
 // function Counter() {
@@ -56,47 +60,34 @@ export default MyList;
 // export default Counter;
 
 
-// function App() {
-//   return (
-//     <>
+function App() {
+  const [keyword, setKeyword] = useState('');
+  const [data, setData] = useState([]);
+  const columns = [ 
+    {field: 'full_name'},
+    {field: 'html_url'},
+    {field: 'owner.login'}
+  ]
 
-//     <h1>Hello World</h1>
-//     <p>This is my First React Componet</p>
-//     </>
+  const fetchData = () => {
+    fetch(`https://api.github.com/search/repositories?q=${keyword}`)
+    .then(response => response.json())
+    .then(data => setData(data.items))
+    .catch(err => console.error(err))
+  }
 
-//   );
-// }
-//   const [keyword, setKeyword] = useState('');
-//   const [data, setData] = useState([]);
+  return (
+    <div className="App">
+      <input value={keyword} onChange={e => setKeyword(e.target.value)} />
+      <button onClick={fetchData}>Fetch</button>
+      <div className='ag-theme-material' style={{height : 500, wibth: '90%'}}>
+        <AgGridReact
+        rowData={data}
+        columnDefs={columns}
+        />
+    </div>
+    </div>
+  );
+}
 
-//   const fetchData = () => {
-//     fetch(`https://api.github.com/search/repositories?q=${keyword}`)
-//     .then(response => response.json())
-//     .then(data => setData(data.items))
-//     .catch(err => console.error(err))
-//   }
-
-//   return (
-//     <div className="App">
-//       <input value={keyword} 
-//         onChange={e => setKeyword(e.target.value)} />
-//       <button onClick={fetchData}>Fetch</button>
-//       <table style={{margin: 'auto'}}>
-//         <tbody>
-//           {
-//             data.map(repo => 
-//               <tr key={repo.id}>
-//                 <td>{repo.full_name}</td>
-//                 <td>
-//                   <a href={repo.html_url}>{repo.html_url}</a>
-//                 </td>
-//               </tr>  
-//             )
-//           }
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-
-// export default App;
+export default App;
