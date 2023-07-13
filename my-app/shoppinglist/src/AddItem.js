c451a2104a5bfa48f25aeb2c766d0fc8b34bde54
Attackdie <pkpk85@naver.com>
2023-07-13 15:55:49 +0900

    리액트 MUI 컴포넌트 및 두개의 js 파일 랜더링 임포트 테스트

--- my-app/shoppinglist/src/AddItem.js
+++ my-app/shoppinglist/src/AddItem.js
@@ -1,15 +1,20 @@
 import React, {useState} from "react";
-import { Button } from "@mui/material/Button";
-import { TextField } from "@mui/material/TextField";
-import { Dialog } from "@mui/material/Dialog";
-import { DialogActions } from "@mui/material/DialogActions";
-import { DialogContent } from "@mui/material/DialogContent";
-import { DialogTitle } from "@mui/material/DialogTitle";
+import { Button } from "@mui/material/";
+import { TextField } from "@mui/material/";
+import { Dialog } from "@mui/material/";
+import { DialogActions } from "@mui/material/";
+import { DialogContent } from "@mui/material/";
+import { DialogTitle } from "@mui/material/";
 
 function AddItem(props) {
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => {
         setOpen(true);
+    }
+    const addItem = () => {
+        props.addItem(item);
+        setItem({product: '', amount: ''}); //텍스트 필드를 비움
+        handleClose();
     }
     const handleClose = () => {
         setOpen(false);
@@ -33,6 +38,9 @@ function AddItem(props) {
                     <TextField value={item.product} margin="dense"
                     onChange={handleChange} name="product" 
                     label="Product" fullWidth />
+                     <TextField value={item.product} margin="dense"
+                    onChange={handleChange} name="amount" 
+                    label="Amount" fullWidth />
                 </DialogContent>
                 <DialogActions>
                     <Button onClick={handleClose}>
